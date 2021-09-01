155bddd3a2cf4bfbac96d83d04cd37bf574a53e5
LongLe <xuanlongvts@gmail.com>
2021-09-01 18:35:40 +0700

    fix titile

--- dapp/fe/src/_commComp/sidebar/index.tsx
+++ dapp/fe/src/_commComp/sidebar/index.tsx
@@ -7,10 +7,17 @@ import ListItemText from '@material-ui/core/ListItemText';
 import Divider from '@material-ui/core/Divider';
 import Typography from '@material-ui/core/Typography';
 
+import { darkThemeModes } from 'themes/const';
 import navConfig from './consts';
 
 const useStyles = makeStyles((theme: Theme) =>
     createStyles({
+        title: {
+            color:
+                theme.palette.type === darkThemeModes.dark
+                    ? theme.palette.primary.dark
+                    : theme.palette.secondary.contrastText,
+        },
         order: {
             width: 25,
             height: 25,
@@ -33,7 +40,7 @@ const Sidebar = () => {
 
     return (
         <aside>
-            <Typography variant="h5" align="center" gutterBottom>
+            <Typography variant="h4" align="center" gutterBottom className={classes.title}>
                 Solana DApp
             </Typography>
             <Divider />
