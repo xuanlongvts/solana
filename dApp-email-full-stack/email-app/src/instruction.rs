d89767f2e288a9b01071792e46df1d6a1ee7016f
LongLe <xuanlongvts@gmail.com>
2021-10-20 18:36:26 +0700

    done tet for instruction and state

--- dApp-email-full-stack/email-app/src/instruction.rs
+++ dApp-email-full-stack/email-app/src/instruction.rs
@@ -3,7 +3,7 @@ use crate::state::Email;
 use borsh::BorshDeserialize;
 use solana_program::program_error::ProgramError;
 
-#[derive(Debug)]
+#[derive(Debug, PartialEq)]
 pub enum EMailInstruction {
 	InitAccount,               // init account
 	SendEmail { mail: Email }, // AccountInfo for sender, receiver
@@ -19,5 +19,63 @@ impl EMailInstruction {
 			},
 			_ => return Err(InvalidInstruction.into()),
 		})
+	}
+}
+
+#[cfg(test)]
+mod test {
+	use super::*;
+	use borsh::BorshSerialize;
+	use solana_program::{borsh::get_instance_packed_len, pubkey::Pubkey};
+
+	#[test]
+	fn test_init_endpoint() {
+		let data: Vec<u8> = vec![0];
+		let mail_instruction = EMailInstruction::unpack(&data).unwrap();
+
+		assert_eq!(mail_instruction, EMailInstruction::InitAccount);
+	}
+
+	#[test]
+	fn test_send_endpoint() {
+		let test_mail = Email {
+			id: String::from("00000000-0000-0000-0000-000000000000"),
+			from_address: Pubkey::default().to_string(),
+			to_address: Pubkey::default().to_string(),
+			subject: String::from("Hey Long"),
+			body: String::from("Body text with some character"),
+			send_date: String::from("20/10/2021, 09:00:00 am"),
+		};
+
+		// exam: data = vec![1, 1, 1, 1, 1, 1] in case get_instance_packed_len = 4;
+		/*
+			// get_instance_packed_len ===> Ok(n)
+			// Ok(2).unwrap() = 2;
+			let x: Result<u32, u32> = Ok(2);
+			println!("x: {:?}", x.unwrap() + 1); = 3
+
+			let y: Option<usize> = Some(3);
+			println!("y: {:?}", y.unwrap() + 1); = 4
+		*/
+		let mut data: Vec<u8> = vec![1; get_instance_packed_len(&test_mail).unwrap() + 1];
+		test_mail.serialize(&mut &mut data[1..]).unwrap();
+
+		let mail_instruction = EMailInstruction::unpack(&data).unwrap();
+		assert_eq!(
+			mail_instruction,
+			EMailInstruction::SendEmail {
+				mail: test_mail.clone()
+			}
+		);
+
+		match mail_instruction {
+			EMailInstruction::InitAccount => (),
+			EMailInstruction::SendEmail { mail } => {
+				assert_eq!(mail.from_address, test_mail.from_address);
+				assert_eq!(mail.to_address, test_mail.to_address);
+				assert_eq!(mail.subject, test_mail.subject);
+				assert_eq!(mail.body, test_mail.body);
+			}
+		}
 	}
 }
--- dApp-email-full-stack/email-app/src/state.rs
+++ dApp-email-full-stack/email-app/src/state.rs
@@ -20,3 +20,72 @@ pub struct EmailAccount {
 pub struct DataLength {
 	pub length: u32,
 }
+
+#[cfg(test)]
+mod test {
+	use super::*;
+	use solana_program::{borsh::get_instance_packed_len, pubkey::Pubkey};
+
+	#[test]
+	fn test_email() {
+		let mail = Email {
+			id: String::from("00000000-0000-0000-0000-000000000000"),
+			from_address: Pubkey::default().to_string(),
+			to_address: Pubkey::default().to_string(),
+			subject: String::from("Hey Long"),
+			body: String::from("Body text with some character"),
+			send_date: String::from("20/10/2021, 09:00:00 am"),
+		};
+		let mut tmp_slice = [0; 500];
+		mail.serialize(&mut &mut tmp_slice[..]).unwrap(); // serialize data to Unit8Array
+
+		println!("tmp_slice: {:?}", tmp_slice);
+
+		/* tmp_slice is
+		[36, 0, 0, 0, 48, 48, 48, 48, 48, 48, 48, 48, 45, 48, 48, 48, 48, 45, 48, 48, 48, 48, 45, 48, 48, 48, 48, 45, 48, 48, 48, 48, 48, 48, 48, 48, 48, 48, 48, 48, 32, 0, 0, 0, 49, 49, 49, 49, 49, 49, 49, 49, 49, 49, 49, 49, 49, 49, 49, 49, 49, 49, 49, 49, 49, 49, 49, 49, 49, 49, 49, 49, 49, 49, 49, 49, 32, 0, 0, 0, 49, 49, 49, 49, 49, 49, 49, 49, 49, 49, 49, 49, 49, 49, 49, 49, 49, 49, 49, 49, 49, 49, 49, 49, 49, 49, 49, 49, 49, 49, 49, 49, 8, 0, 0, 0, 72, 101, 121, 32, 76, 111, 110, 103, 29, 0, 0, 0, 66, 111, 100, 121, 32, 116, 101, 120, 116, 32, 119, 105, 116, 104, 32, 115, 111, 109, 101, 32, 99, 104, 97, 114, 97, 99, 116, 101, 114, 23, 0, 0, 0, 50, 48, 47, 49, 48, 47, 50, 48, 50, 49, 44, 32, 48, 57, 58, 48, 48, 58, 48, 48, 32, 97, 109, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+		*/
+
+		let length = get_instance_packed_len(&mail).unwrap();
+
+		println!("length: {:?}", length); // get length from data tmp;
+		let mail = Email::try_from_slice(&tmp_slice[..length]).unwrap(); // deserialize data to text
+		assert_eq!(mail.subject, "Hey Long");
+	}
+
+	#[test]
+	fn test_mail_account() {
+		let mail = Email {
+			id: String::from("00000000-0000-0000-0000-000000000000"),
+			from_address: Pubkey::default().to_string(),
+			to_address: Pubkey::default().to_string(),
+			subject: String::from("Hey Long"),
+			body: String::from("Body text with some character"),
+			send_date: String::from("20/10/2021, 09:00:00 am"),
+		};
+		let mail_account = EmailAccount {
+			inbox: vec![mail],
+			sent: Vec::new(),
+		};
+		let mut temp_slice = [0; 500];
+		mail_account.serialize(&mut &mut temp_slice[..]).unwrap();
+
+		let length = get_instance_packed_len(&mail_account).unwrap();
+		let mail_account = EmailAccount::try_from_slice(&temp_slice[..length]).unwrap();
+		assert_eq!(mail_account.inbox[0].subject, "Hey Long");
+	}
+
+	#[test]
+	fn test_data_length() {
+		let data_length = DataLength { length: 5 };
+
+		let mut temp_slice = [0; 4];
+		data_length.serialize(&mut &mut temp_slice[..]).unwrap();
+
+		assert_eq!(temp_slice, [5, 0, 0, 0]);
+
+		let data_length = DataLength::try_from_slice(&temp_slice[..4]).unwrap();
+		assert_eq!(data_length.length, 5);
+	}
+}
+
+// printlnt in text run with:   cargo test -- --nocapture
