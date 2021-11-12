32fd85b597b8c8a3dbf95caef1208b3cfcb64913
LongLe <xuanlongvts@gmail.com>
2021-11-12 19:24:47 +0700

    structer page

--- crow-funding/fe/src/_config/index.ts
+++ crow-funding/fe/src/_config/index.ts
@@ -2,24 +2,43 @@ const env = process.env.REACT_APP_ENV;
 
 export enum ENUM_envName {
     dev = 'dev',
-    qc = 'qc',
-    uat = 'uat',
+    test = 'test',
     production = 'production',
-    sandbox = 'sandbox',
 }
+
+export enum SOLANA_PROTOCOLS {
+    API_SERVER = 'API_SERVER',
+    HUB_RPC = 'HUB_RPC',
+}
+
 type T_envName = {
     dev: string;
-    qc: string;
-    uat: string;
+    test: string;
     production: string;
-    sandbox: string;
 };
 export const envName: T_envName = {
     [ENUM_envName.dev]: 'dev',
-    [ENUM_envName.qc]: 'qc',
-    [ENUM_envName.uat]: 'uat',
+    [ENUM_envName.test]: 'qc',
     [ENUM_envName.production]: 'production',
-    [ENUM_envName.sandbox]: 'sandbox',
+};
+
+export const Config = {
+    [envName.dev]: {
+        [SOLANA_PROTOCOLS.API_SERVER]: 'https://api.devnet.solana.com',
+        [SOLANA_PROTOCOLS.HUB_RPC]: '',
+    },
+    [envName.test]: {
+        [SOLANA_PROTOCOLS.API_SERVER]: 'https://api.testnet.solana.com',
+        [SOLANA_PROTOCOLS.HUB_RPC]: '',
+    },
+    [envName.production]: {
+        [SOLANA_PROTOCOLS.API_SERVER]: 'https://api.mainnet-beta.solana.com',
+        [SOLANA_PROTOCOLS.HUB_RPC]: '',
+    },
+};
+
+export const getConfig = (envParams = env, protocol = SOLANA_PROTOCOLS.API_SERVER) => {
+    return Config[envParams as string][protocol];
 };
 
 export default env;
--- crow-funding/fe/src/app/pages/createCampain/index.tsx
+++ crow-funding/fe/src/app/pages/createCampain/index.tsx
@@ -1,96 +1,11 @@
-import { ChangeEvent, SyntheticEvent, FC, ReactElement, useState } from 'react';
-import TextField from '@mui/material/TextField';
-import Button from '@mui/material/Button';
-import { useForm } from 'react-hook-form';
-import { yupResolver } from '@hookform/resolvers/yup';
+import CreateCaimpain from 'app/comps/createCampain';
 
-import { names, T_InforCampaiin } from './consts';
-import { CampainSchema } from './validate';
-
-const CreateCampain = (): ReactElement => {
-    const [inforCampain, setInforCampain] = useState<T_InforCampaiin>();
-
-    const {
-        register,
-        handleSubmit,
-        watch,
-        formState: { errors },
-    } = useForm<T_InforCampaiin>({
-        mode: 'onBlur',
-        resolver: yupResolver(CampainSchema),
-    });
-
-    const hanldeInforCampain = (e: ChangeEvent<HTMLInputElement>) => {
-        const { name, value } = e.target;
-        setInforCampain({
-            ...inforCampain,
-            [name]: value,
-        });
-    };
-
-    const onSubmitForm = () => {
-        console.log('handleSubmit');
-    };
-
-    const disabledBtn = !!(
-        errors[names.title] ||
-        errors[names.des] ||
-        errors[names.imgLink] ||
-        !watch()[names.title] ||
-        !watch()[names.des] ||
-        !watch()[names.imgLink]
-    );
-
+const CreateCaimpainPage = () => {
     return (
         <section>
-            <form>
-                <TextField
-                    required
-                    fullWidth
-                    variant="outlined"
-                    margin="normal"
-                    id="title"
-                    type="text"
-                    label="Title"
-                    {...register(names.title)}
-                    error={!!errors[names.title]}
-                    helperText={errors[names.title]?.message}
-                />
-                <TextField
-                    required
-                    fullWidth
-                    variant="outlined"
-                    type="text"
-                    margin="normal"
-                    id="description"
-                    label="Description"
-                    {...register(names.des)}
-                    error={!!errors[names.des]}
-                    helperText={errors[names.des]?.message}
-                />
-                <TextField
-                    required
-                    fullWidth
-                    variant="outlined"
-                    type="text"
-                    margin="normal"
-                    label="Image Link"
-                    {...register(names.imgLink)}
-                    error={!!errors[names.imgLink]}
-                    helperText={errors[names.imgLink]?.message}
-                />
-                <Button
-                    variant="outlined"
-                    color="primary"
-                    size="large"
-                    disabled={disabledBtn}
-                    onClick={handleSubmit(onSubmitForm)}
-                >
-                    Primary
-                </Button>
-            </form>
+            <CreateCaimpain />
         </section>
     );
 };
 
-export default CreateCampain;
+export default CreateCaimpainPage;
